190f3f631c6ef0ad80e7484064387a42e7ac6aab
Teemu Kupari <tkupari@gmail.com>
2020-08-13 10:21:36 +0300

    Get port from env

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ app.get('/info', (_req, res) => {
   res.send(`<p>Phonebook has info for ${persons.length} people</p>${new Date()}</p></p>`)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
