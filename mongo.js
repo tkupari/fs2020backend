e7634297366b39336761d15cdb601a07c09c4260
Teemu Kupari <tkupari@gmail.com>
2020-08-13 13:16:23 +0300

    3.12 command line mongo tool

---
+++ mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+if(process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.da0wg.mongodb.net/phonebook-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+
+if(process.argv.length > 3) {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4]
+  })
+
+  person.save().then(_result => {
+    console.log(`added ${person.name} number ${person.number} to phonebook`)
+    mongoose.connection.close()
+  })
+} else {
+
+  Person.find({}).then(result => {
+    console.log('phonebook:')
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+}
