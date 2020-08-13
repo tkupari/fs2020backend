8218215334099ec25b4c075daaee49a31a53874e
Teemu Kupari <tkupari@gmail.com>
2020-08-13 10:31:55 +0300

    Add frontend build

---
+++ build/precache-manifest.502d7e8b5b0445816fb8258bcdbb1acd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e272ad137b9d98044aa730d1f6d666b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1feca67f8d8ab4228c9f",
+    "url": "/static/css/main.c6ffa42e.chunk.css"
+  },
+  {
+    "revision": "dbaf8825220e57e56d18",
+    "url": "/static/js/2.ad435a02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ad435a02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1feca67f8d8ab4228c9f",
+    "url": "/static/js/main.31bcda6b.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
